e1c509a2ed78b8bfad896ec3de0218a67ba94a09
rkyarc <arickyh1@gmail.com>
2026-08-17 21:58:06 +0700

    feat(codewars): solve exercise 0001

---
+++ codewars/8-kyu/0001-Function1HelloWorld.ts
@@ -0,0 +1,19 @@
+/*
+Exercise 0001 - Function 1 - Hello World
+---------------------------------------------------------
+Task:
+Create a function called `greet` that returns "hello world!".
+
+Example:
+greet() --> "hello world!"
+
+Link:
+https://www.codewars.com/kata/523b4ff7adca849afe000035
+---------------------------------------------------------*/
+
+function greet(): string {
+    return "hello world!";
+}
+
+
+console.log(greet());
